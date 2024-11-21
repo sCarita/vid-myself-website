ab1431f4abc6625cb958ff1f5b8e1701514e4405
oZeca <ze.lupicaetano@gmail.com>
2024-11-21 11:47:26 +0000

    fix: missing lines in cta section

--- src/app/components/cta.tsx
+++ src/app/components/cta.tsx
@@ -5,7 +5,16 @@ const CTA = () => {
   return (
     <section className="bg-gradient-to-t from-[#CBED5E] to-[#fff]">
       <Section className="flex justify-center items-center h-[215px]">
-        <Button title="Get Started" variant="secondary" href="/upload" />
+        <div className="flex items-center gap-4 md:gap-8 w-full">
+          <div className="w-full border-b border-black" />
+          <Button
+            customClass="whitespace-nowrap"
+            title="Get Started"
+            variant="secondary"
+            href="/upload"
+          />
+          <div className="w-full border-b border-black " />
+        </div>
       </Section>
     </section>
   );
