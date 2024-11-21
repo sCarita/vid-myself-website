34ad8e0a8df23ccaa854ddd2551920337aba282f
oZeca <ze.lupicaetano@gmail.com>
2024-11-21 11:35:41 +0000

    fix: hero section layout

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -4,9 +4,9 @@ import Section from "./section";
 
 const Hero = () => {
   return (
-    <Section className="flex flex-col md:flex-row gap-6 h-auto md:h-[50vh] justify-center">
+    <Section className="flex flex-col md:flex-row gap-6 h-auto md:h-[50vh] justify-center items-center">
       <div className="flex flex-col gap-6 flex-1 justify-center">
-        <h2 className="font-bold text-[40px] md:text-[64px] uppercase leading-[48px] md:leading-[80px]">
+        <h2 className="font-bold text-[40px] md:text-[50px] lg:text-[64px] uppercase leading-[48px] md:leading-[60px] lg:leading-[80px]">
           Turn your photos into a fashion video
         </h2>
         <p className="font-bold text-[20px] md:text-[25px] uppercase">
@@ -16,8 +16,8 @@ const Hero = () => {
           <Button title="Get started" href="/upload" />
         </div>
       </div>
-      <div className="grid grid-cols-3 gap-4 flex-1 h-full">
-        <div className="flex justify-center">
+      <div className="grid grid-cols-3 gap-4 flex-1">
+        <div className="flex md:justify-end">
           <Image
             src="/placeholder.png"
             alt="example"
@@ -26,7 +26,7 @@ const Hero = () => {
             height={266}
           />
         </div>
-        <div className="flex flex-col justify-center">
+        <div className="flex flex-col justify-center md:items-end gap-4 lg:gap-10">
           <Image
             src="/placeholder.png"
             alt="example"
@@ -42,7 +42,7 @@ const Hero = () => {
             height={266}
           />
         </div>
-        <div className="flex flex-col justify-center">
+        <div className="flex flex-col justify-center md:items-end gap-4 lg:gap-10">
           <Image
             src="/placeholder.png"
             alt="example"
